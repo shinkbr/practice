1657a6f6a76d1f49033314f386a594d69f2f957c
Shintaro Kobori <shinkbr@gmail.com>
2021-01-18 00:00:34 +0900

    refactor

--- prime-factorization/go/main.go
+++ prime-factorization/go/main.go
@@ -33,7 +33,7 @@ func main() {
       n = n / 2
     }
 
-    for i := 3; i <= int(math.Ceil(math.Sqrt(float64(n)))); i += 2 {
+    for i := 3; i <= int(math.Sqrt(float64(n))); i += 2 {
       for n % i == 0 {
         print_with_space(i, &print_space)
         n = n / i
